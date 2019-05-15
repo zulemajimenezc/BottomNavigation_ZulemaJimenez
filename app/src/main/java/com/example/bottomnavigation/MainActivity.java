87482ba29da0bd673163271dbc8b74107f09adaf
Zulema Jimenez <zcjime02@gmail.com>
2019-05-15 02:14:47 -0500

    3 Agregando Fragmentos (se agregaron 4 fragmentos para cambiar de vista presionando un elemento del bottom navigation)

--- app/src/main/java/com/example/bottomnavigation/MainActivity.java
+++ app/src/main/java/com/example/bottomnavigation/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.bottomnavigation;
 import android.os.Bundle;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.annotation.NonNull;
@@ -24,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
         toolbar.setTitle("Shop");
+        loadFragment(new StoreFragment());
     }
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
@@ -35,19 +37,34 @@ public class MainActivity extends AppCompatActivity {
             switch (item.getItemId()) {
                 case R.id.navigation_shop:
                     toolbar.setTitle("Shop");
+                    fragment =  new StoreFragment();
+                    loadFragment(fragment);
                     return true;
                 case R.id.navigation_gifts:
                     toolbar.setTitle("My gifts");
+                    fragment =  new GiftsFragment();
+                    loadFragment(fragment);
                     return true;
                 case R.id.navigation_cart:
                     toolbar.setTitle("Cart");
+                    fragment =  new CartFragment();
+                    loadFragment(fragment);
                     return true;
                 case R.id.navigation_profile:
                     toolbar.setTitle("Profile");
+                    fragment =  new ProfileFragment();
+                    loadFragment(fragment);
                     return true;
             }
             return false;
         }
     };
+
+    public void loadFragment(Fragment fragment){
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_container,fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
 
 }
